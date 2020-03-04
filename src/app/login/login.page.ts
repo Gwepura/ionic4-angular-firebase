27ecc88fbf156e4d2defea17de38b1ab2a23c7b7
Gwepura <aketani32@gmail.com>
2020-03-04 16:04:07 +0200

    Login Form Validation Part-1

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
+  loginForm: FormGroup;
+  email: FormControl;
+  password: FormControl;
+
   constructor() { }
 
   ngOnInit() {
+    this.createFormControl();
+    this.createForm();
   }
 
+  createFormControl() {
+    this.email = new FormControl('', [
+      Validators.required,
+      Validators.email
+    ]);
+
+    this.password = new FormControl('', [
+      Validators.required,
+      Validators.minLength(5)
+    ]);
+  }
+
+  createForm() {
+    this.loginForm = new FormGroup({
+      email: this.email,
+      password: this.password
+    });
+    this.loginForm.valueChanges.subscribe(data => this.onFormValueChanged(data));
+  }
+
+
+  onFormValueChanged(data) {
+    console.log('data', data);
+    console.log('this.loginForm', this.loginForm);
+  }
 }
