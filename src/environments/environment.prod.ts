38757501e194d319aee4d7fbe0336aaa4720ef02
Gwepura <aketani32@gmail.com>
2020-03-04 15:41:56 +0200

    Create Login form

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyA5sYGqRIyFhJQYenYHjnMh_bgm68Kd_QQ",
+    authDomain: "ionic4-angular-firebase.firebaseapp.com",
+    databaseURL: "https://ionic4-angular-firebase.firebaseio.com",
+    projectId: "ionic4-angular-firebase",
+    storageBucket: "ionic4-angular-firebase.appspot.com",
+    messagingSenderId: "1081123911538",
+    appId: "1:1081123911538:web:61e637a084910566750feb"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyA5sYGqRIyFhJQYenYHjnMh_bgm68Kd_QQ",
+    authDomain: "ionic4-angular-firebase.firebaseapp.com",
+    databaseURL: "https://ionic4-angular-firebase.firebaseio.com",
+    projectId: "ionic4-angular-firebase",
+    storageBucket: "ionic4-angular-firebase.appspot.com",
+    messagingSenderId: "1081123911538",
+    appId: "1:1081123911538:web:61e637a084910566750feb"
+  }
+
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
